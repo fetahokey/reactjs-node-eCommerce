a0a0f3f8aa7cc3b73837fa5c19de5d7b40dd1a53
Fetah .OK <32603079+fetahokey@users.noreply.github.com>
2021-10-05 11:47:34 +0100

    create the compo Announcement.tsx

---
+++ src/components/Announcement.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 150 px;
+    background-color: black;
+    color: white;
+`;
+
+const Announcement = () => {
+    return (
+        <Container>
+            Announcement Slider !
+        </Container>
+    )
+}
+
+export default Announcement
